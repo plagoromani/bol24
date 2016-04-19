4ce0fd5a135bd12c5ad9bc61cab72d1f68f1080b
Pablite5 <Pablite5@GAMING>
2016-04-19 09:02:29 +0200

    commit82

---
+++ src/bol24/Bol24.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol24;
+
+/**
+ *
+ * @author Pablite5
+ */
+public class Bol24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
